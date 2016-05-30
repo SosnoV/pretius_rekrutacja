136347dcf346bb9c34ead3fe6a7c33ad182d4124
Rafał Sosnowski <rsosnowski@rivetgroup.eu>
2016-05-30 23:16:23 +0200

    Przygotowanie środowiska - metoda main

---
+++ src/main/java/pl/sosna/Main.java
@@ -0,0 +1,13 @@
+package pl.sosna;
+
+/**
+ * <p>Created by rsosn on 30.05.2016.</p>
+ *
+ * @author rsosn
+ */
+public class Main {
+
+	public static void main(String [ ] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/main/java/pl/sosna/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>Created by rsosn on 30.05.2016.</p>
+ *
+ * @author rsosn
+ */
+package pl.sosna;
