074b6aad53099e01cf8329fb9e04d6ef0f25f793
Rafał Sosnowski <rsosnowski@rivetgroup.eu>
2016-05-31 12:00:37 +0200

    Zadanie rekrutacyjne

--- src/main/java/pl/sosna/Main.java
+++ src/main/java/pl/sosna/Main.java
@@ -1,5 +1,15 @@
 package pl.sosna;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.stream.Stream;
+
 /**
  * <p>Created by rsosn on 30.05.2016.</p>
  *
@@ -8,6 +18,42 @@ package pl.sosna;
 public class Main {
 
 	public static void main(String [ ] args) {
-		System.out.println("Hello World!");
+		String filePath = args[0];
+		String atSeparator = "@";
+		String colonSeparator = ":";
+		String wordFilter = "amount";
+		String plnFilter = "PLN";
+		final BigDecimal[] sum = {BigDecimal.ZERO};
+
+		MathContext mc = new MathContext(4, RoundingMode.HALF_UP);
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream(filePath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+		String strLine;
+		try {
+			while ((strLine = br.readLine()) != null)   {
+				if (strLine.isEmpty()) {
+					continue;
+				}
+				Stream.of(strLine.split(atSeparator))
+						.filter(s -> s.contains(wordFilter))
+						.flatMap(amount -> Stream.of(amount.split(colonSeparator)))
+						.filter(res -> !res.contains(wordFilter)).forEach(last -> {
+							String afterReplace = last.replace(plnFilter, "").replace(",", ".").trim();
+							Double money = Double.parseDouble(afterReplace);
+							sum[0] = sum[0].add(BigDecimal.valueOf(money), mc);
+						});
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Suma kwot wynosi: " + sum[0].doubleValue());
+
+
 	}
 }
